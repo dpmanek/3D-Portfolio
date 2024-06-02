c60e9c7fd4f0b42df9c838b7d770af60fba89a6a
Deep Manek <dpmanek@gmail.com>
2024-06-02 14:57:58 -0400

    Navbar done, working on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../styles';
+import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+	return (
+		<section className="relative w-full h-screen mx-auto">
+			Hero
+			<div
+				className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+			>
+				<div className="flex flex-col justify-center items-center mt-5">
+					<div className="w-5 h-5 rounded-full bg-[#915eff]" />
+					<div className="w-1 h-40 sm:h-80  violet-gradient" />
+				</div>
 
-export default Hero
+				<div>
+					<h1 className={`${styles.heroHeadText}`}>
+						Hi, I'm <span className="text-[#915eff]">Deep</span>
+					</h1>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Hero;
