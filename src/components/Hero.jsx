d07ad970bc13b2c8562faba07d8c2a9384b9cf65
Deep Manek <dpmanek@gmail.com>
2024-06-05 00:27:15 -0400

    added mediaquery for mobile screen and motion framer scroller

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,6 @@ import { ComputersCanvas } from './canvas';
 const Hero = () => {
 	return (
 		<section className="relative w-full h-screen mx-auto">
-			Hero
 			<div
 				className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
 			>
@@ -18,7 +17,28 @@ const Hero = () => {
 					<h1 className={`${styles.heroHeadText}`}>
 						Hi, I'm <span className="text-[#915eff]">Deep</span>
 					</h1>
+					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
+						I develop web applications and websites.
+						<br className="sm:block hidden" /> I am a Full Stack Developer.
+					</p>
 				</div>
+			</div>
+			<ComputersCanvas />
+
+			<div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+				<a hrref="#about">
+					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+						<motion.dev
+							animate={{ y: [0, 24, 0] }}
+							transition={{
+								duration: 1.5,
+								repeat: Infinity,
+								repeatType: 'loop',
+							}}
+							className="w-3 h-3 rounded-full bg-secondary mb-1"
+						/>
+					</div>
+				</a>
 			</div>
 		</section>
 	);
